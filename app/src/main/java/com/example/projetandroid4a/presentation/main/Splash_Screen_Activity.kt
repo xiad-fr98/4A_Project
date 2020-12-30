f49123accdb7bebbb838327b78df303772a1e0bf
xiad-fr98 <xia@et.esiea.fr>
2020-12-30 22:50:24 +0100

    Finitions

--- app/src/main/java/com/example/projetandroid4a/presentation/main/Splash_Screen_Activity.kt
+++ app/src/main/java/com/example/projetandroid4a/presentation/main/Splash_Screen_Activity.kt
@@ -1,9 +1,9 @@
-package com.example.projetandroid4a
+package com.example.projetandroid4a.presentation.main
 
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.projetandroid4a.presentation.main.MainActivity
+import com.example.projetandroid4a.R
 import kotlinx.android.synthetic.main.activity_splash__screen_.*
 
 class Splash_Screen_Activity : AppCompatActivity() {
